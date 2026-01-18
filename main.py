f6a75bee15670301b6c159a0611c6e7ccc750675
Tanay <tanaybhattacharjee30@gmail.com>
2026-01-18 21:16:57 +0530

    updated

--- main.py
+++ main.py
@@ -132,20 +132,20 @@ async def create_bank_loan_receipt(
     try:
         final_attachment_path = None
         
-        # If file is uploaded, upload to Firebase
-        if attachment and attachment.filename:
-            file_content = await attachment.read()
-            file_url = upload_file_to_firebase(file_content, attachment.filename)
-            if file_url:
-                final_attachment_path = file_url
-            else:
-                raise HTTPException(
-                    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                    detail="Failed to upload attachment to Firebase"
-                )
-        # If attachment_path (URL) is provided directly, use it
-        elif attachment_path:
-            final_attachment_path = attachment_path
+        # # If file is uploaded, upload to Firebase
+        # if attachment and attachment.filename:
+        #     file_content = await attachment.read()
+        #     file_url = upload_file_to_firebase(file_content, attachment.filename)
+        #     if file_url:
+        #         final_attachment_path = file_url
+        #     else:
+        #         raise HTTPException(
+        #             status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+        #             detail="Failed to upload attachment to Firebase"
+        #         )
+        # # If attachment_path (URL) is provided directly, use it
+        # elif attachment_path:
+        #     final_attachment_path = attachment_path
         
         # Create receipt data
         receipt_data = {
@@ -157,7 +157,7 @@ async def create_bank_loan_receipt(
             "amount": amount,
             "receipt_mode": receipt_mode,
             "remarks": remarks,
-            "attachment_path": final_attachment_path
+            # "attachment_path": final_attachment_path
         }
         
         db_receipt = BankLoanReceipt(**receipt_data)
